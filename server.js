e40d7ec949693d05de53ecb20ac024db5fb819cb
abdul Barre <abarre2009@gmail.com>
2020-03-14 15:30:05 -0500

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var mongoose = require("mongoose");
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.urlencoded({ extended: true }));
